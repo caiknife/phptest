d8f64e4ae6b67f09b2935ac34206c1c64889dd04
caiknife <caiknife@foxmail.com>
2013-10-16 09:23:57 +0800

    2013年 10月 16日 星期三 09:23:57 CST

---
+++ sphinx/1.php
@@ -0,0 +1,22 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+
+require_once 'Zend/Loader/Autoloader.php';
+Zend_Loader_Autoloader::getInstance();
+
+$sph = new SphinxClient();
+$sph->setServer('localhost', 9312);
+$sph->setMatchMode(SPH_MATCH_ANY);
+
+$result = $sph->query("doc test");
+// Zend_Debug::dump($result['matches']);
+Zend_Debug::dump($result);
+
+// $result = $sph->query("doc test");
+// Zend_Debug::dump($result['matches']);
+// Zend_Debug::dump($result);
+
+// $result = $sph->query("你好");
+// Zend_Debug::dump($result['matches']);
+// Zend_Debug::dump($result);
