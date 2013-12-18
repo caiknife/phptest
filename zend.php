5e5d931d857df72199a7b47ac61953393f40ab4c
caiknife <caiknife@foxmail.com>
2013-12-18 14:12:34 +0800

    2013年 12月 18日 星期三 14:12:34 CST

---
+++ zend.php
@@ -0,0 +1,46 @@
+<?php
+require_once 'Zend/Loader/Autoloader.php';
+Zend_Loader_Autoloader::getInstance();
+
+//$date = Zend_Date::now();
+//Zend_Debug::dump($date->toString());
+//Zend_Debug::dump($date->isLeapYear());
+//Zend_Debug::dump($date->isToday());
+//Zend_Debug::dump($date->isYesterday());
+//Zend_Debug::dump($date->isTomorrow());
+//
+//$currency = new Zend_Currency();
+//Zend_Debug::dump($currency->toCurrency());
+//Zend_Debug::dump($currency->toCurrency(999));
+//Zend_Debug::dump($currency->getCurrencyList());
+//$currency->setValue(11233);
+//Zend_Debug::dump($currency->toCurrency());
+//
+//$fig = new Zend_Text_Figlet();
+//Zend_Debug::dump($fig->render('CaiKnife'));
+//
+//$uri = Zend_Uri::factory('http://www.caiknife.com/index.php?name=caiknife&power=acjaif#id');
+//Zend_Debug::dump($uri);
+
+$request = new Zend_Http_Client();
+$request->setConfig(array(
+    'useragent' => 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:21.0) Gecko/20100101 Firefox/21.0',
+    ''
+));
+
+$request->setUri('http://www.php.net/');
+
+Zend_Debug::dump($request->getUri(true));
+
+$json = <<<JSON
+var people = {
+    "name": "caiknife",
+    "hello": {
+        "foo": "1",
+        "bar": "2"
+    }
+};
+JSON;
+
+Zend_Debug::dump($json);
+
