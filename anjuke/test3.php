72d1f1f0696616ddd91ba6dffa9e0d2b6e973321
caiknife <caiknife@foxmail.com>
2014-02-21 08:39:57 +0800

    2014年 02月 21日 星期五 08:39:57 CST

---
+++ anjuke/test3.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caiknife
+ * Date: 14-2-21
+ * Time: 上午8:38
+ */
+
+$var1 = 60.06;
+$var2 = 60.01 + 0.01 + 0.01 + 0.01 + 0.01 + 0.01;
+
+var_dump($var1 === $var2);
