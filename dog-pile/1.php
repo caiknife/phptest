be21cddb67bc121f01cf382b68da148fa1bb8a1f
caizhijiang <54394636@qq.com>
2014-04-06 16:42:35 +0800

    memcache demo

--- dog-pile/1.php
+++ dog-pile/1.php
@@ -1,11 +1,11 @@
 <?php
 require_once 'zend_autoload.php';
 
-$mc = new Memcache();
+$mc = new Memcached();
 $mc->addServers(array(
     array('127.0.0.1', 11211, 40),
     array('127.0.0.1', 11212, 30),
-    array('127.0.0.1', 11213, 30)
+    array('127.0.0.1', 11213, 30),
 ));
 $data = $mc->get('cached_key');
 if ($mc->getResultCode() === Memcached::RES_NOTFOUND) {
