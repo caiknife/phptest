3de6c3d8aa169370736b407bbcd9a9fff5b9fd9a
caiknife <caiknife@foxmail.com>
2016-05-17 16:22:25 +0800

    add phpinfo

--- phpinfo.php
+++ phpinfo.php
@@ -1,18 +1,9 @@
 <?php
 /**
  * Created by PhpStorm.
-<<<<<<< HEAD
  * User: carvincai
  * Date: 2016/5/17
  * Time: 16:15
  */
 
 phpinfo();
-=======
- * User: caiknife
- * Date: 16/4/10
- * Time: 19:13
- */
-
-phpinfo();
->>>>>>> origin/master
